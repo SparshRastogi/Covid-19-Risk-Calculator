7c73435677c42185e73c77fdd9fda375f0ed4bfd
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2022-04-11 18:59:22 +0530

    Update Cases.py

--- Modular Version/Data Collection/Cases.py
+++ Modular Version/Data Collection/Cases.py
@@ -1,31 +1,21 @@
 import requests
 import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
-columns = df.columns
-
-df['date'] = pd.to_datetime(df['date'])
-df['year'] = df['date'].dt.year
-df['month'] = df['date'].dt.month
-
-cases2021 = df.loc[df['year'] == 2021]
-
-cases2021
-
-counties = list(cases2021['fips'].unique())
-
-counties
-
-cases2021.rename(columns = {'fips':'FIPS CODE'}, inplace = True)
-
-for j in range(1,10):
+def cases_dataset(month,year):
+  df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+  columns = df.columns
+  df['date'] = pd.to_datetime(df['date'])
+  df['year'] = df['date'].dt.year
+  df['month'] = df['date'].dt.month
+  cases2021 = df.loc[df['year'] == year]
+  counties = list(cases2021['fips'].unique())
+  cases2021.rename(columns = {'fips':'FIPS CODE'}, inplace = True)
   dfco = pd.DataFrame(columns = columns)
   dfco = dfco.drop(['fips'],axis = 1)
   for i in counties:    
-     data = pd.DataFrame(cases2021.loc[(cases2021['FIPS CODE'] == i) & (cases2021['month'] == j)])
+     data = pd.DataFrame(cases2021.loc[(cases2021['FIPS CODE'] == i) & (cases2021['month'] == month)])
      cases = ['cases','deaths']     
      cases1 = ['FIPS CODE','cases','deaths']
-     d = pd.DataFrame(cases2021.loc[(cases2021['FIPS CODE'] == i) & (cases2021['month'] == j)])
+     d = pd.DataFrame(cases2021.loc[(cases2021['FIPS CODE'] == i) & (cases2021['month'] == month)])
      d = d.drop((cases),axis=1)
      d = d.drop_duplicates(subset = 'FIPS CODE')
      da =pd.DataFrame(data[cases1].mean())
@@ -35,5 +25,5 @@ for j in range(1,10):
      dfco = pd.concat(objs,axis = 0)
      dfco.reset_index(drop = True,inplace = True)
      print(dfco)
-  name = '/content/drive/MyDrive/Colab Notebooks/Cases 2021 Month' + str(j) +'.csv'
-  dfco.to_csv(name)
+  name = f'/content/drive/MyDrive/Colab Notebooks/Cases 2021 Month{month}.csv'
+  dfco.to_csv(name,index = False)
