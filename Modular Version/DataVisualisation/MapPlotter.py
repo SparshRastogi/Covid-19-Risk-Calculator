57da3d5264f640d95fd9525d6dc8bac298f77363
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2022-04-12 19:34:34 +0530

    Update MapPlotter.py

--- Modular Version/DataVisualisation/MapPlotter.py
+++ Modular Version/DataVisualisation/MapPlotter.py
@@ -1,18 +1,18 @@
-mport numpy as np
-import plotly.offline as offline
-from sklearn.preprocessing import MinMaxScaler
-import plotly
-import plotly.figure_factory as ff
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+def plotter(month,year):
+  import numpy as np
+  import plotly.offline as offline
+  from sklearn.preprocessing import MinMaxScaler
+  import plotly
+  import plotly.figure_factory as ff
+  from urllib.request import urlopen
+  import json
+  with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
-import pandas as pd
-scaler = MinMaxScaler((0,100))
+  import pandas as pd
+  scaler = MinMaxScaler((0,100))
 
-import plotly.express as px
-for i in range(1,10):
-  df =  pd.read_csv('https://raw.githubusercontent.com/SparshRastogi/Covid-19-Risk-Calculator/main/2021%20Month' + str(i) + '%20Risk.csv')
+  import plotly.express as px
+  df =  pd.read_csv(f'https://raw.githubusercontent.com/SparshRastogi/Covid-19-Risk-Calculator/main/{year}%20Month{month}%20Risk.csv')
   df['FIPS CODE']=df['FIPS CODE'].apply(lambda x: '{0:0>5}'.format(x) )
   df['risk'] = scaler.fit_transform(np.array(df['risk']).reshape(-1,1))
   fig = px.choropleth(df,geojson = counties,locations='FIPS CODE', color='risk',
